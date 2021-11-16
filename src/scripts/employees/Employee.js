68a096d4cf05fe4ad7059accb3cad9eaf081a21b
Andrew Birt <abirt1986@gmail.com>
2021-11-16 14:57:12 -0500

    completed chapter 'Creating Departments'

--- src/scripts/employees/Employee.js
+++ src/scripts/employees/Employee.js
@@ -1,7 +1,8 @@
-export const Employee = (emp, potato) => {
+export const Employee = (emp, potato, personalHell) => {
   return `
     <article>
       <h3>${emp.firstName} ${emp.lastName}</h3>
+      <p>Department: ${personalHell.name}</p>
       <p>Equipment: ${potato.model}</p>
       <p>Year: ${potato.year}</p>
     </article>
--- src/scripts/employees/EmployeeList.js
+++ src/scripts/employees/EmployeeList.js
@@ -1,20 +1,24 @@
 import { getComputers, useComputers } from "../computers/ComputerData.js"
+import { getDepartments, useDepartments } from "../departments/DepartmentData.js";
 import { Employee } from "./Employee.js";
 import { getEmployees, useEmployees } from "./EmployeeData.js"
 
 export const EmployeeList = () => {
   getEmployees()
   .then(getComputers)
+  .then(getDepartments)
   .then(() => {
     const employees = useEmployees();
     const computers = useComputers();
-    document.querySelector('#container').innerHTML = render(employees, computers);
+    const departments = useDepartments();
+    document.querySelector('#container').innerHTML = render(employees, computers, departments);
   });
 }
 
-const render = (employeeCollection, computerCollection) => {
+const render = (employeeCollection, computerCollection, departmentCollection) => {
   return employeeCollection.map( emp => {
     const employeeComputer = computerCollection.find(computer => computer.id === emp.computerId);
-    return Employee(emp, employeeComputer);
+    const employeeDepartment = departmentCollection.find(department => department.id === emp.departmentId);
+    return Employee(emp, employeeComputer, employeeDepartment);
   }).join('');
 }
