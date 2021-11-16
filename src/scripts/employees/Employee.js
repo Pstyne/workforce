65dbd2cfb051c16cd8f9435a65392063d39a32cc
Andrew Birt <abirt1986@gmail.com>
2021-11-16 15:04:10 -0500

    complete chapter 'Growing the Business'

--- src/scripts/employees/Employee.js
+++ src/scripts/employees/Employee.js
@@ -1,8 +1,9 @@
-export const Employee = (emp, potato, personalHell) => {
+export const Employee = (emp, potato, personalHell, dumpster) => {
   return `
     <article>
       <h3>${emp.firstName} ${emp.lastName}</h3>
       <p>Department: ${personalHell.name}</p>
+      <p>Location: ${dumpster.name}</p>
       <p>Equipment: ${potato.model}</p>
       <p>Year: ${potato.year}</p>
     </article>
--- src/scripts/employees/EmployeeList.js
+++ src/scripts/employees/EmployeeList.js
@@ -1,5 +1,6 @@
 import { getComputers, useComputers } from "../computers/ComputerData.js"
 import { getDepartments, useDepartments } from "../departments/DepartmentData.js";
+import { getLocations, useLocations } from "../locations/LocationData.js";
 import { Employee } from "./Employee.js";
 import { getEmployees, useEmployees } from "./EmployeeData.js"
 
@@ -7,18 +8,21 @@ export const EmployeeList = () => {
   getEmployees()
   .then(getComputers)
   .then(getDepartments)
+  .then(getLocations)
   .then(() => {
     const employees = useEmployees();
     const computers = useComputers();
     const departments = useDepartments();
-    document.querySelector('#container').innerHTML = render(employees, computers, departments);
+    const locations = useLocations();
+    document.querySelector('#container').innerHTML = render(employees, computers, departments, locations);
   });
 }
 
-const render = (employeeCollection, computerCollection, departmentCollection) => {
+const render = (employeeCollection, computerCollection, departmentCollection, locationCollection) => {
   return employeeCollection.map( emp => {
     const employeeComputer = computerCollection.find(computer => computer.id === emp.computerId);
     const employeeDepartment = departmentCollection.find(department => department.id === emp.departmentId);
-    return Employee(emp, employeeComputer, employeeDepartment);
+    const employeeLocation = locationCollection.find(location => location.id === emp.locationId);
+    return Employee(emp, employeeComputer, employeeDepartment, employeeLocation);
   }).join('');
 }
